2d4ee6ee43a32bdbe9e1f0ec7257881731e7ce6e
Matthew Sells <matthew.m.sells@gmail.com>
2021-09-08 20:14:00 -0500

    remove unused imports

--- src/pages/recipes/Show.tsx
+++ src/pages/recipes/Show.tsx
@@ -1,20 +1,15 @@
 import { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router';
 
 import * as Layout from '@/components/Layout';
-import * as Modal from '@/components/Modal';
 import * as Recipe from '@/components/Recipe';
-import * as Text from '@/components/Text';
 import { ApiContext } from '@/context';
-import useActiveAccount from '@/hooks/useActiveAccount';
 import { IDParams } from '@/types/global';
 import { Recipe as RecipeType } from '@/types/recipe';
 
 function Show() {
 	const api = useContext(ApiContext);
-	const { t } = useTranslation();
 	const { id } = useParams<IDParams>();
 
 	// TODO: Create something to make these requests
