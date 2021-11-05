9e0afccf8b8e066d9df4eff23e4d6179f7c04e2a
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-04 19:50:28 -0500

    update env config

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+console.log('PORT IS', process.env.PORT);
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
