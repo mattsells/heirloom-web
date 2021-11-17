5e5ffd448bca2ed73f1c553da806b8bff219b45c
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-17 08:42:19 -0600

    add tailwind css

--- craco.config.js
+++ craco.config.js
@@ -8,6 +8,11 @@ module.exports = {
 			},
 		},
 	},
+	style: {
+		postcss: {
+			plugins: [require('tailwindcss'), require('autoprefixer')],
+		},
+	},
 	webpack: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
