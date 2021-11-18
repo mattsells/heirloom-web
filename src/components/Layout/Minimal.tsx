c38a83983e95d22692c8d1706b1f4f7557e942f8
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-17 18:13:07 -0600

    update panel styles

--- src/components/Layout/Minimal.tsx
+++ src/components/Layout/Minimal.tsx
@@ -1,27 +1,11 @@
 import { ReactElement, ReactNode } from 'react';
-import { createUseStyles } from 'react-jss';
-
-import { Mint } from '@/variables/colors';
 
 type Props = {
 	children: ReactNode;
 };
 
-const useStyles = createUseStyles(
-	{
-		root: {
-			backgroundColor: Mint.light,
-			backgroundImage: `linear-gradient(${Mint.light}, ${Mint.regular})`,
-			height: '100%',
-		},
-	},
-	{ name: 'LayoutMinimal' }
-);
-
 function Minimal({ children }: Props): ReactElement<Props> {
-	const classes = useStyles();
-
-	return <div className={classes.root}>{children}</div>;
+	return <div className="bg-green-200 h-full">{children}</div>;
 }
 
 export default Minimal;
--- src/components/Panel/Content.tsx
+++ src/components/Panel/Content.tsx
@@ -1,25 +1,11 @@
 import { ReactElement, ReactNode } from 'react';
-import { createUseStyles } from 'react-jss';
-
-import { Space } from '@/variables/space';
 
 type Props = {
 	children: ReactNode;
 };
 
-const useStyles = createUseStyles(
-	{
-		root: {
-			padding: Space.thick,
-		},
-	},
-	{ name: 'PanelContent' }
-);
-
 function Content({ children }: Props): ReactElement<Props> {
-	const classes = useStyles();
-
-	return <div className={classes.root}>{children}</div>;
+	return <div className="p-6">{children}</div>;
 }
 
 export default Content;
