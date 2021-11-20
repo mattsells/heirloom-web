d61925f6be6ca7572e36e79d1bf7c02c6dacdc66
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-20 09:29:53 -0600

    update static pages

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,28 +1,8 @@
-import { createUseStyles } from 'react-jss';
-
 import * as LoadingIcons from '@/components/Loading';
-import { Mint } from '@/variables';
-
-const useStyles = createUseStyles(
-	{
-		root: {
-			alignItems: 'center',
-			backgroundColor: Mint.light,
-			display: 'flex',
-			height: '100%',
-			justifyContent: 'center',
-		},
-	},
-	{
-		name: 'Loading',
-	}
-);
 
 function Loading() {
-	const styles = useStyles();
-
 	return (
-		<div className={styles.root}>
+		<div className="flex items-center bg-green-300 h-full justify-center">
 			<LoadingIcons.Spinner />
 		</div>
 	);
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,29 +1,12 @@
 import { useTranslation } from 'react-i18next';
-import { createUseStyles } from 'react-jss';
 
 import * as Text from '@/components/Text';
-
-const useStyles = createUseStyles(
-	{
-		root: {
-			alignItems: 'center',
-			display: 'flex',
-			flex: 1,
-			justifyContent: 'center',
-		},
-	},
-	{
-		name: 'NotFound',
-	}
-);
 
 function NotFound() {
 	const { t } = useTranslation();
 
-	const styles = useStyles();
-
 	return (
-		<div className={styles.root}>
+		<div className="flex items-center bg-green-300 h-full justify-center">
 			<Text.Header>{t('not-found.title')}</Text.Header>
 		</div>
 	);
