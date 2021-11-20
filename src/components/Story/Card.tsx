f36efd543eac4c1494480e69492c555d469657e1
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-20 08:35:36 -0600

    update story card styles

--- src/components/Story/Card.tsx
+++ src/components/Story/Card.tsx
@@ -1,49 +1,35 @@
+import classNames from 'classnames';
 import { ReactElement } from 'react';
-import { createUseStyles } from 'react-jss';
 
 import * as Frame from '@/components/Frame';
 import { Story } from '@/types/story';
-import { Radius, Shadow, Speed } from '@/variables';
 
 type Props = {
 	story: Story;
 };
 
-const useStyles = createUseStyles(
-	{
-		root: {
-			border: 'none',
-			borderRadius: Radius.narrow,
-			boxShadow: Shadow.lightest,
-			display: 'block',
-			overflow: 'hidden',
-			transform: 'scale(0.98)',
-			transition: `transform ${Speed.regular} linear, box-shadow ${Speed.regular} linear`,
-
-			'&:hover': {
-				boxShadow: Shadow.light,
-				transform: 'scale(1.0)',
-			},
-		},
-
-		image: {
-			height: '100%',
-			objectFit: 'cover',
-			width: '100%',
-		},
-	},
-	{ name: 'StoryCard' }
-);
-
 function Card({ story }: Props): ReactElement<Props> {
-	const classes = useStyles(story as any);
-
 	return (
-		<a className={classes.root} href={story.imageUrlMedium}>
+		<a
+			className={classNames(
+				'border-none',
+				'rounded',
+				'shadow',
+				'block',
+				'overflow-hidden',
+				'transform',
+				'scale-95',
+				'hover:scale-100',
+				'transition-all',
+				'duration-100',
+				'ease-linear'
+			)}
+			href={story.imageUrlMedium}
+		>
 			<Frame.Square>
 				<img
 					alt={story.name}
-					className={classes.image}
+					className="h-full w-full object-cover"
 					src={story.imageUrlMedium}
 				/>
 			</Frame.Square>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,8 +8,8 @@ routes.add('default', '/');
 routes.add('home', '/');
 routes.add('login');
 routes.add('recipes');
-routes.add('recipe', '/recipe/:id');
-routes.add('recipe.edit', '/recipe/:id/edit');
+routes.add('recipe', '/recipes/:id');
+routes.add('recipe.edit', '/recipes/:id/edit');
 routes.add('registration', '/join');
 
 export default routes;
