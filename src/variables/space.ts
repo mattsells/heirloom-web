7f4b4e465e0f4c875a14202c7adbfb84e44e0754
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-20 09:26:44 -0600

    remove variables folder

--- src/variables/space.ts
+++
@@ -1,9 +0,0 @@
-export enum Space {
-	none = '0',
-	narrow = '0.4rem',
-	thin = '0.8rem',
-	regular = '1.2rem',
-	thick = '1.6rem',
-	wide = '2.0rem',
-	extraWide = '3.0rem',
-}
--- src/variables/transitions.ts
+++
@@ -1,4 +0,0 @@
-export enum Speed {
-	fast = '0.1s',
-	regular = '0.2s',
-}
